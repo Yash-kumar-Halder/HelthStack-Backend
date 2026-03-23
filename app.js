50a62bb66cab45a7f2fc46b9d94d61fb7ba2bb0e
Yash Kumar Halder <yashkumar667453@gmail.com>
2026-03-23 21:43:19 +0530

    init: initialize the project

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv"
+
+dotenv.config();
+const app = express();
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Server running"
+    })
+})
+
+export default app;
