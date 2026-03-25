7c01fb2746c56c741c730afff11e2af4c2d413fe
Yash Kumar Halder <yashkumar667453@gmail.com>
2026-03-25 22:44:17 +0530

    chore: configure prettier, es-lint, lint-staged, husky

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-import express from "express";
-import dotenv from "dotenv"
+import express from 'express';
+import dotenv from 'dotenv';
 
 dotenv.config();
 const app = express();
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
     res.status(200).json({
         success: true,
-        message: "Server running"
-    })
-})
+        message: 'Server running',
+    });
+});
 
 export default app;
