8e885947adfcd23f458a5bbaea98c1a517962180
Yash Kumar Halder <yashkumar667453@gmail.com>
2026-03-31 18:41:34 +0530

    feat: add role repository

---
+++ src/modules/role/role.repository.js
@@ -0,0 +1,21 @@
+import { RoleModel } from './role.model';
+
+export class RoleRepostory {
+    async createRole({ role, description }) {
+        return await RoleModel.create({
+            name: role,
+            description,
+        });
+    }
+    async deleteRoleById(roleId) {
+        return await RoleModel.findByIdAndDelete(roleId);
+    }
+
+    async updateRoleById(roleId, data) {
+        return await RoleModel.findByIdAndUpdate(
+            roleId,
+            { $set: data },
+            { new: true, runValidators: true },
+        );
+    }
+}
